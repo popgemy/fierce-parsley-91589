20025196b68271e9d3d42536cb8fba4e4f29b783
Stackbit Code Editor <projects@stackbit.com>
2021-10-16 01:53:31 +0000

    SectionPricing.js: changed

--- src/components/SectionPricing.js
+++ src/components/SectionPricing.js
@@ -21,7 +21,9 @@ export default class SectionPricing extends React.Component {
                         {subtitle && <div className="plan-subtitle">{subtitle}</div>}
                         {price && <div className="plan-price">{price}</div>}
                         {price && <div className="plan-price">{price}</div>}
-                        
+                                                {price && <div className="plan-price">{price}</div>}
+
+
                     </div>
                     {details && <div className="plan-content">{markdownify(details)}</div>}
                     {actions && (
